38486d909c6fd4b0d35fe795ac396de6e96889e7
ibukunoluwanap <promisenap2001@gmail.com>
2022-12-13 02:05:14 +0200

    refactored assets dir

--- lib/src/credit_cardfm/credit_cardfm.dart
+++ lib/src/credit_cardfm/credit_cardfm.dart
@@ -2,11 +2,15 @@ import 'package:credit_card_flutterme/src/credit_cardfm/widgets/card_body.dart';
 import 'package:flutter/material.dart';
 
 class CreditCardFM extends StatefulWidget {
+  ///
+  /// <div align="center">
+  /// <h2>Credit Card Flutterme</h2>
   /// Creates a customizable credit card widget with validations.
   ///
   /// <img src="https://raw.githubusercontent.com/fluttermeorg/.github/main/profile/dashatar.png" width="100%">
   ///
   /// Made By Flutterme @https://github.com/fluttermeorg
+  /// </div>
   const CreditCardFM({
     super.key,
     this.width = 430,
@@ -21,52 +25,70 @@ class CreditCardFM extends StatefulWidget {
     this.gradient,
   });
 
+  /// #### Width
   /// This controls the width of the credit card.
   ///
   /// Note that the width can be your device width, however know that width
   /// restriction has been set between [300]-[430]
   ///
+  /// <div align="center">
   /// <img src="https://raw.githubusercontent.com/ibukunoluwanap/credit_card_flutterme/main/images/docs/width.png" width="80%">
+  /// </div>
   final double width;
 
+  /// #### Height
   /// This controls the height of the credit card.
   ///
   /// Note that the height can be your device height, however know that height
   /// restriction has been set between [200]-[250]
   ///
+  /// <div align="center">
   /// <img src="https://raw.githubusercontent.com/ibukunoluwanap/credit_card_flutterme/main/images/docs/height.png" width="80%">
+  /// </div>
   final double height;
 
+  /// #### Padding
   /// Creates an inner space (padding) between the credit card border and the inner widgets
   ///
+  /// <div align="center">
   /// <img src="https://raw.githubusercontent.com/ibukunoluwanap/credit_card_flutterme/main/images/docs/padding.png" width="80%">
+  /// </div>
   final EdgeInsetsGeometry padding;
 
+  /// #### Margin
   /// Creates an outer space (margin) between the credit card border and the outside layouts.
   ///
+  /// <div align="center">
   /// <img src="https://raw.githubusercontent.com/ibukunoluwanap/credit_card_flutterme/main/images/docs/margin.png" width="80%">
+  /// </div>
   final EdgeInsetsGeometry margin;
 
+  /// #### Color
   /// To set the background color of the card. This can not be a gradient.
   /// To set a gradient background color use the `backgroundGradient`.
-  /// *backgroundColor* and *backgroundGradient* can't be set at same time.
+  /// `backgroundColor` and `backgroundGradient` can't be set at same time.
   final Color color;
 
-  /// This is to set a background image for the card. When set, *backgroundColor*
-  /// and *backgroundGradient* have no effects.
+  /// #### Image
+  /// This is to set a background image for the card. When set, `backgroundColor`
+  /// and `backgroundGradient` have no effects.
   final DecorationImage? image;
 
+  /// #### Border
   /// Adjustment of card border.
   final BoxBorder? border;
 
+  /// #### Border Radius
   /// Adjustment of card border radius.
   final BorderRadiusGeometry? borderRadius;
 
+  /// #### Box Shadow
   /// Allows the controls of card elevation and shadow directions.
   final List<BoxShadow>? boxShadow;
 
-  /// This is to set a background gradient color for the card. *backgroundColor* and
-  /// *backgroundGradient* can't be set at same time.
+  /// #### Gradient
+  /// This is to set a background gradient color for the card. `backgroundColor` and
+  /// `backgroundGradient` can't be set at same time.
   final Gradient? gradient;
 
   @override
@@ -87,6 +109,7 @@ class _CreditCardFMState extends State<CreditCardFM> {
     );
   }
 
+  /// Return an immutable card constraints
   BoxConstraints cardConstraint() {
     return const BoxConstraints(
       maxWidth: 430,
@@ -96,6 +119,7 @@ class _CreditCardFMState extends State<CreditCardFM> {
     );
   }
 
+  /// Return all card decoration
   Decoration cardDecoration() {
     return BoxDecoration(
       color: widget.color,
@@ -108,11 +132,14 @@ class _CreditCardFMState extends State<CreditCardFM> {
   }
 }
 
+/// #### Card Type
 /// used in the selection of card type during the card validation stage.
 /// This have all the type of supported cards in a `enum`.
 ///
-/// LIST OF SUPPORTED CARDS
+/// ##### List of supported cards
+/// <div align="center">
 /// <img src="https://raw.githubusercontent.com/ibukunoluwanap/credit_card_flutterme/main/images/docs/support_cards.png" width="80%">
+/// </div>
 enum CardType {
   amex,
   chip,
