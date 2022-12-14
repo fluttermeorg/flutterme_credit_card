e513021352e7e560815ffaedd1ed0666c1669668
ibukunoluwanap <promisenap2001@gmail.com>
2022-12-14 09:46:22 +0200

    updated docs

--- lib/src/credit_cardfm/credit_cardfm.dart
+++ lib/src/credit_cardfm/credit_cardfm.dart
@@ -32,7 +32,7 @@ class CreditCardFM extends StatefulWidget {
   /// restriction has been set between [300]-[430]
   ///
   /// <div align="center">
-  /// <img src="https://raw.githubusercontent.com/ibukunoluwanap/credit_card_flutterme/main/images/docs/width.png" width="80%">
+  /// <img src="https://raw.githubusercontent.com/ibukunoluwanap/credit_card_flutterme/main/images/docs/width.png" width="50%">
   /// </div>
   final double width;
 
@@ -43,7 +43,7 @@ class CreditCardFM extends StatefulWidget {
   /// restriction has been set between [200]-[250]
   ///
   /// <div align="center">
-  /// <img src="https://raw.githubusercontent.com/ibukunoluwanap/credit_card_flutterme/main/images/docs/height.png" width="80%">
+  /// <img src="https://raw.githubusercontent.com/ibukunoluwanap/credit_card_flutterme/main/images/docs/height.png" width="50%">
   /// </div>
   final double height;
 
@@ -51,7 +51,7 @@ class CreditCardFM extends StatefulWidget {
   /// Creates an inner space (padding) between the credit card border and the inner widgets
   ///
   /// <div align="center">
-  /// <img src="https://raw.githubusercontent.com/ibukunoluwanap/credit_card_flutterme/main/images/docs/padding.png" width="80%">
+  /// <img src="https://raw.githubusercontent.com/ibukunoluwanap/credit_card_flutterme/main/images/docs/padding.png" width="50%">
   /// </div>
   final EdgeInsetsGeometry padding;
 
@@ -59,7 +59,7 @@ class CreditCardFM extends StatefulWidget {
   /// Creates an outer space (margin) between the credit card border and the outside layouts.
   ///
   /// <div align="center">
-  /// <img src="https://raw.githubusercontent.com/ibukunoluwanap/credit_card_flutterme/main/images/docs/margin.png" width="80%">
+  /// <img src="https://raw.githubusercontent.com/ibukunoluwanap/credit_card_flutterme/main/images/docs/margin.png" width="50%">
   /// </div>
   final EdgeInsetsGeometry margin;
 
@@ -72,23 +72,44 @@ class CreditCardFM extends StatefulWidget {
   /// #### Image
   /// This is to set a background image for the card. When set, `backgroundColor`
   /// and `backgroundGradient` have no effects.
+  ///
+  /// <div align="center">
+  /// <img src="https://raw.githubusercontent.com/ibukunoluwanap/credit_card_flutterme/main/images/docs/image.png" width="50%">
+  /// </div>
   final DecorationImage? image;
 
   /// #### Border
   /// Adjustment of card border.
+  ///
+  /// <div align="center">
+  /// <img src="https://raw.githubusercontent.com/ibukunoluwanap/credit_card_flutterme/main/images/docs/border.png" width="50%">
+  /// </div>
   final BoxBorder? border;
 
   /// #### Border Radius
   /// Adjustment of card border radius.
+  ///
+  /// <div align="center">
+  /// <img src="https://raw.githubusercontent.com/ibukunoluwanap/credit_card_flutterme/main/images/docs/border_radius.png" width="50%">
+  /// </div>
   final BorderRadiusGeometry? borderRadius;
 
   /// #### Box Shadow
   /// Allows the controls of card elevation and shadow directions.
+  ///
+  ///
+  /// <div align="center">
+  /// <img src="https://raw.githubusercontent.com/ibukunoluwanap/credit_card_flutterme/main/images/docs/box_shadow.png" width="50%">
+  /// </div>
   final List<BoxShadow>? boxShadow;
 
   /// #### Gradient
   /// This is to set a background gradient color for the card. `backgroundColor` and
   /// `backgroundGradient` can't be set at same time.
+  ///
+  /// <div align="center">
+  /// <img src="https://raw.githubusercontent.com/ibukunoluwanap/credit_card_flutterme/main/images/docs/gradient.png" width="50%">
+  /// </div>
   final Gradient? gradient;
 
   @override
@@ -138,7 +159,7 @@ class _CreditCardFMState extends State<CreditCardFM> {
 ///
 /// ##### List of supported cards
 /// <div align="center">
-/// <img src="https://raw.githubusercontent.com/ibukunoluwanap/credit_card_flutterme/main/images/docs/support_cards.png" width="80%">
+/// <img src="https://raw.githubusercontent.com/ibukunoluwanap/credit_card_flutterme/main/images/docs/support_cards.png" width="50%">
 /// </div>
 enum CardType {
   amex,
